3b72a8c227492c28e8c321c01ce1ccce883a394a
Cole Winslow <colethomaswinslow7@gmail.com>
2021-04-19 15:31:00 -0700

    added product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  const productData = await Product.findAll({include: [Category, Tag]});
   // be sure to include its associated Category and Tag data
+  res.json(productData)
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  const product = await Product.findByPk(req.params.id,  {include: [Category, Tag]});
   // be sure to include its associated Category and Tag data
+  res.json(product)
 });
 
 // create new product
@@ -91,6 +95,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try{
+    const destroyedProduct = await Product.destroy({where: { id }});
+    res.json(destroyedProduct);
+  }
+  catch(err){
+    console.log(err.message)
+    res.json(err)
+  }
 });
 
 module.exports = router;
