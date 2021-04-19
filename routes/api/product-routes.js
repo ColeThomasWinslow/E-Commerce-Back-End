367925850831adf14307ff72bb5bbfb3b3cc4861
Cole Winslow <colethomaswinslow7@gmail.com>
2021-04-19 15:35:37 -0700

    added product find by id create and delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -93,7 +93,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try{
     const destroyedProduct = await Product.destroy({where: { id }});
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,22 +3,54 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({include: [Product]});
+    res.json(tagData)
+  }
+  catch (err) {
+    console.log(err.message)
+    res.status(400).end(err.message)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  try {
+    const tag = await Tag.findByPk(req.params.id, {include: [Product]});
+    res.json(tag)
+  }
+  catch (err) {
+    console.log(err.message)
+    res.status(400).end(err.message)
+  }
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.json(newTag)
+  }
+  catch (err) {
+    console.log(err.message)
+    res.status(400).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updatedTag = await Tag.update(body, { where: { id } })
+    res.json(updatedTag)
+  }
+  catch (err) {
+    console.log(err.message)
+    res.status(400).end(err.message)
+  }
 });
 
 router.delete('/:id', (req, res) => {
